e10629be84acda894ceca0f4b900b8de15fd5c0a
whq361 <whq361@qq.com>
2018-09-27 16:37:19 +0800

    平台基本UI

--- src/components/checkboxStepper/index.js
+++ src/components/checkboxStepper/index.js
@@ -13,156 +13,156 @@ class CheckboxStepper extends React.Component {
   constructor(props) {
     super(props);
     autoBind(this);
-    this.state=({
+    this.state = ({
       data: props.data || null,
-      checkeAll:props.checkeAll,
+      checkeAll: props.checkeAll,
     })
- 
+
   }
 
   componentWillMount() {
 
   }
-  componentWillUpdate(){
+  componentWillUpdate() {
 
   }
-  componentDidUpdate(){
-  
+  componentDidUpdate() {
+
   }
   componentDidMount() {
   }
 
   componentWillReceiveProps(nextProps) {
-    const data =nextProps.data;
-    if(data){
-     
+    const data = nextProps.data;
+    if (data) {
+
       this.setState({
         ...this.state,
         data
       })
     }
-   
-  
-  
-   
+
+
+
+
   }
 
-  componentWillUnmount() {}
+  componentWillUnmount() { }
 
   checkNumCLick(e) {
     const data = this.state.data;
-    const keyId =e.target.id;
-    data.map((item,index)=>{
-      if(item.key === keyId){
-        if(item.switch){
-          item.switch=false;
-        }else{
-          item.switch=true;
-          if(item.num > 0){
-            item.num = item.num ;
-          }else{
+    const keyId = e.target.id;
+    data.map((item, index) => {
+      if (item.key === keyId) {
+        if (item.switch) {
+          item.switch = false;
+        } else {
+          item.switch = true;
+          if (item.num > 0) {
+            item.num = item.num;
+          } else {
             item.num = 1;
           }
         }
       }
 
     })
-   this.setState({
-     ...this.state,
-     data,
-   })
-   this.props.getData(data);
+    this.setState({
+      ...this.state,
+      data,
+    })
+    this.props.getData(data);
 
   }
-  onSub(e){
+  onSub(e) {
     e.nativeEvent.stopImmediatePropagation();
     const data = this.state.data;
-    const keyId =e.currentTarget.id;
-    data.map((item,index)=>{
-      if(item.key === keyId){
-        if(item.is_required !== '1'){
-          if(item.num === 0){
+    const keyId = e.currentTarget.id;
+    data.map((item, index) => {
+      if (item.key === keyId) {
+        if (item.is_required !== '1') {
+          if (item.num === 0) {
             return
-           }else{
-             item.num = item.num-1;
-             if( item.num===0){
-              item.switch=false;
-             }
-           }
-        }else{
-          if(item.num === 1){
-            item.switch=false;
-           }else{
-             item.num = item.num-1;
-           }
+          } else {
+            item.num = item.num - 1;
+            if (item.num === 0) {
+              item.switch = false;
+            }
+          }
+        } else {
+          if (item.num === 1) {
+            item.switch = false;
+          } else {
+            item.num = item.num - 1;
+          }
         }
       }
 
     })
-   this.setState({
-     ...this.state,
-     data,
-   })
-   this.props.getData(data);
+    this.setState({
+      ...this.state,
+      data,
+    })
+    this.props.getData(data);
   }
-  onAdd(e){
+  onAdd(e) {
     const data = this.state.data;
-    const keyId =e.target.id;
-    data.map((item,index)=>{
-      if(item.key === keyId){
-          item.switch=true;
-          item.num= item.num+1;
+    const keyId = e.target.id;
+    data.map((item, index) => {
+      if (item.key === keyId) {
+        item.switch = true;
+        item.num = item.num + 1;
       }
 
     })
-   this.setState({
-     ...this.state,
-     data,
-   })
-   this.props.getData(data);
+    this.setState({
+      ...this.state,
+      data,
+    })
+    this.props.getData(data);
   }
   render() {
-    const data =this.state.data;
+    const data = this.state.data;
     const props = {
       ...this.props,
     };
- 
+
     return (
       <div className="component-CheckboxStepper" >
-            {
-              data?data.map((item,index)=>{
-                const info =item;
-               return(
-                <div>
-                  <div  className={classnames({
-                    'component-CheckboxStepper-checkbox': true,
-                    'component-CheckboxStepper-checkbox-false':item.is_required === '0' && !item.switch,
-                    'component-CheckboxStepper-checkbox-disable': item.is_required === '1',
-                    'component-CheckboxStepper-checkbox-true': item.is_required === '0' && item.switch,
-                    'component-CheckboxStepper-checkbox-all' : item.is_required === '0' && this.state.checkeAll,
-         
-                  })}>
-                    <label htmlFor={item.key}>
-                        <input id={item.key} checked={item.switch} type="checkbox" key={item.key} onChange={this.checkNumCLick} disabled={item.is_required === '1'?true:false}/>
-                        <div className="labelname">{item.label}</div>
-                        <div className="price">
-                          <span>{item.amount}</span>
-                        </div>
-                    </label>
-                    <div className="container">
-                            <div className="sub" onClick={this.onSub}  id={item.key}>-</div>
-                            <div className="view">{item.num}</div>
-                            <div className="add" onClick={this.onAdd}  id={item.key}>+</div>
-                        
-                    </div>
-                  </div>
-                  <div className="line1px"/>
-                </div>
-               )
+        {
+          data ? data.map((item, index) => {
+            const info = item;
+            return (
+              <div>
+                <div className={classnames({
+                  'component-CheckboxStepper-checkbox': true,
+                  'component-CheckboxStepper-checkbox-false': item.is_required === '0' && !item.switch,
+                  'component-CheckboxStepper-checkbox-disable': item.is_required === '1',
+                  'component-CheckboxStepper-checkbox-true': item.is_required === '0' && item.switch,
+                  'component-CheckboxStepper-checkbox-all': item.is_required === '0' && this.state.checkeAll,
 
-                
-              }):null
-            }
+                })}>
+                  <label htmlFor={item.key}>
+                    <input id={item.key} checked={item.switch} type="checkbox" key={item.key} onChange={this.checkNumCLick} disabled={item.is_required === '1' ? true : false} />
+                    <div className="labelname">{item.label}</div>
+                    <div className="price">
+                      <span>{item.amount}</span>
+                    </div>
+                  </label>
+                  <div className="container">
+                    <div className="sub" onClick={this.onSub} id={item.key}>-</div>
+                    <div className="view">{item.num}</div>
+                    <div className="add" onClick={this.onAdd} id={item.key}>+</div>
+
+                  </div>
+                </div>
+                <div className="line1px" />
+              </div>
+            )
+
+
+          }) : null
+        }
       </div>
     );
   }
--- src/pages/platfrom/information/detail.js
+++ src/pages/platfrom/information/detail.js
@@ -3,9 +3,9 @@ import autoBind from 'react-autobind';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import './detail.css';
-
+import Avatar from '../../../components/avatar/avatar';
 import history from '../../history';
-
+import html2canvas from 'html2canvas';
 import {
   requestSearch,
 } from './index.store';
@@ -14,7 +14,11 @@ class TeamSearchPage extends React.Component {
   constructor(props) {
     super(props);
     autoBind(this);
-   
+    this.state = ({
+      dataUrl: ''
+    })
+    this.certCachet = window.orgInfo.cert_cachet || '/images/my/zdx.png';
+    this.certAuthOrg = window.orgInfo.cert_auth_org || '和众泽益志愿服务中心';
   }
 
   componentWillMount() {
@@ -22,7 +26,8 @@ class TeamSearchPage extends React.Component {
   }
 
   componentDidMount() {
-
+    console.log(this.props.detail)
+    console.log('加载了')
 
   }
 
@@ -31,14 +36,128 @@ class TeamSearchPage extends React.Component {
 
   componentWillUnmount() {
   }
+  htm2Click = () => {
+    var that = this;
+    var shareContent = this.refs['LaunchContent'];
+    var width = shareContent.offsetWidth;
+    var height = shareContent.offsetHeight;
+    var canvas = document.createElement('canvas');
+    var scale = 5;
+    canvas.width = width * scale;
+    canvas.height = height * scale;
+    canvas.getContext("2d").scale(scale, scale);
+    var opts = {
+      scale: scale,
+      canvas: canvas,
+      logging: true,
+      width: width,
+      height: height
+    }
+    html2canvas(shareContent, opts).then(function (canvas) {
+      var dataUrl = canvas.toDataURL('image/jpeg', 1);
 
- 
+      that.setState({
+        dataUrl
+      })
+    });
+  }
+
   render() {
 
     return (
       <div className="page-platfrom-information-detail">
-            111
+        <div className="page-platfrom-information-detail-top">
+          <div className="btn">生成证书</div>
+          <div className="btn" onClick={this.htm2Click}>长按保存服务记录
+          {
+              this.state.dataUrl != '' ? <img src={this.state.dataUrl} className="img" /> : null
+            }
+
+
+          </div>
+        </div>
+        <div className="takeup"></div>
+        <div className="content" ref="LaunchContent">
+          <div className="information">
+            <div className="information-top">
+              <div className="information-main">
+                <div className="information-main-name-container">
+                  <div className="information-main-name">高晓生</div>
+                  <img src="/images/shop/man.png" />
+                </div>
+                <div className="information-main-name-container-fonts">民族：汉族</div>
+                <div className="information-main-name-container-fonts">身份证号：220521987092782</div>
+                <div className="information-main-name-container-fonts">绑定手机：183123456781</div>
+              </div>
+              <Avatar src='/images/shop/man.png' size={{ width: 100, height: 120, radius: 4 }} />
+            </div>
+            <div className="page-platfrom-information-detail-top-item-container">
+              <div className="page-platfrom-information-detail-top-item">
+                <p className="page-platfrom-information-detail-top-item-top"><b className="page-platfrom-information-detail-top-item-num">1</b>个</p>
+                <p className="page-platfrom-information-detail-top-item-bottom">我的团队</p>
+              </div>
+              <div className="page-platfrom-information-detail-top-item">
+                <p className="page-platfrom-information-detail-top-item-top"><b className="page-platfrom-information-detail-top-item-num">2</b>个</p>
+                <p className="page-platfrom-information-detail-top-item-bottom">我的项目</p>
+              </div>
+              <div className="page-platfrom-information-detail-top-item">
+                <p className="page-platfrom-information-detail-top-item-top"><b className="page-platfrom-information-detail-top-item-num">3</b>小时</p>
+                <p className="page-platfrom-information-detail-top-item-bottom">志愿时长</p>
+              </div>
+              <div className="page-platfrom-information-detail-top-item">
+                <p className="page-platfrom-information-detail-top-item-top"><b className="page-platfrom-information-detail-top-item-num">4</b>星币</p>
+                <p className="page-platfrom-information-detail-top-item-bottom">志愿星币</p>
+              </div>
+            </div>
+          </div>
+          <div className="main">
+            <div>
+              <div className="main-item-fonts top">注册平台：中国青年志愿者协会</div>
+              <div className="main-item-fonts">项目名称：益起来！呵护200万年前的湿地精灵</div>
+              <div className="main-item-fonts">发起团队：北京青年志愿者团队</div>
+              <div className="main-item-fonts bottom">获得时长：234小时</div>
+              <div className="line1px" />
+            </div>
+
+            <div>
+              <div className="main-item-fonts">注册平台：中国青年志愿者协会</div>
+              <div className="main-item-fonts">项目名称：益起来！呵护200万年前的湿地精灵</div>
+              <div className="main-item-fonts">发起团队：北京青年志愿者团队</div>
+              <div className="main-item-fonts bottom">获得时长：234小时</div>
+              <div className="line1px" />
+            </div>
+            <div>
+              <div className="main-item-fonts">注册平台：中国青年志愿者协会</div>
+              <div className="main-item-fonts">项目名称：益起来！呵护200万年前的湿地精灵</div>
+              <div className="main-item-fonts">发起团队：北京青年志愿者团队</div>
+              <div className="main-item-fonts bottom">获得时长：234小时</div>
+              <div className="line1px" />
+            </div>
+            <div>
+              <div className="main-item-fonts">注册平台：中国青年志愿者协会</div>
+              <div className="main-item-fonts">项目名称：益起来！呵护200万年前的湿地精灵</div>
+              <div className="main-item-fonts">发起团队：北京青年志愿者团队</div>
+              <div className="main-item-fonts bottom">获得时长：234小时</div>
+              <div className="line1px" />
+            </div>
+            <div>
+              <div className="main-item-fonts">注册平台：中国青年志愿者协会</div>
+              <div className="main-item-fonts">项目名称：益起来！呵护200万年前的湿地精灵</div>
+              <div className="main-item-fonts">发起团队：北京青年志愿者团队</div>
+              <div className="main-item-fonts bottom">获得时长：234小时</div>
+              <div className="line1px" />
+            </div>
+
+          </div>
+          <img src={this.certCachet} className="certCachet" />
+            
+        
+        </div>
+
+
+
       </div>
+
     );
   }
 }
@@ -60,6 +179,7 @@ TeamSearchPage.title = '搜索志愿项目';
 
 export default connect(
   state => ({
+    detail: state.team.detail,
     list: state.platfrom.search,
     user: state.user,
   }),
