e32a88b7965b390a1013a9ce10ade08289cb8fb8
whq361 <whq361@qq.com>
2018-10-18 15:07:56 +0800

    save

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Mon Oct 15 2018 10:11:42 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Thu Oct 18 2018 15:07:13 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.e87db7ff815e1df02241.js"></script>
+  <script src="/dist/main.a839e9dd7fbf8ffd0cc7.js"></script>
 </body>
 
 </html>
