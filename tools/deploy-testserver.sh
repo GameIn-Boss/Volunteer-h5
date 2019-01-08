485e96e8ef7af855e3243cbe90961073eb4dfd27
whq361 <whq361@qq.com>
2019-01-08 19:25:57 +0800

    服务端首页，欠缺定位，城市切换逻辑

--- tools/deploy-testserver.sh
+++
@@ -1,5 +0,0 @@
-git stash
-git pull
-yarn build
-scp -r  ./public/wechat.blade.php miller@39.107.251.62:/data/webapp/volunteer/Volunteer_wechat/resources/views/wechat.blade.php
-scp -r  ./public/* miller@39.107.251.62:/data/webapp/volunteer/Volunteer_wechat/public/
