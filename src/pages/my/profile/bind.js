4c56de63dd6c1637956b6cd1739ef3aeb03829a5
andy136240860 <739917432@qq.com>
2022-09-13 13:32:29 +0800

    bind phone

--- src/pages/my/profile/bind.js
+++ src/pages/my/profile/bind.js
@@ -68,7 +68,11 @@ class BindInfo extends React.Component {
       }
       if (phone && verifyCode) {
         if (Lphone.fetching && !Nphone.fetching && !Nphone.failed) {
-          window.location.replace('/my/profile/detail/user');
+          if (window.orgCode === 'EKQe1wRbJY') {
+            window.location.replace('/');
+          } else {
+            window.location.replace('/my/profile/detail/user');
+          }
           // history.replace('/my/profile/detail/user');
         }
       }
@@ -91,9 +95,9 @@ class BindInfo extends React.Component {
     });
   }
   onTextChanged() {
-    const phone = this.userphone.value.replace(/(^\s+)|(\s+$)/g, '');
-    const verifyCode = this.usercode.value.replace(/(^\s+)|(\s+$)/g, '');
-    const captcha = this.captcha.value.replace(/(^\s+)|(\s+$)/g, '');
+    const phone = this.userphone && this.userphone.replace(/(^\s+)|(\s+$)/g, '');
+    const verifyCode = this.usercode && this.usercode.replace(/(^\s+)|(\s+$)/g, '');
+    const captcha = this.captcha && this.captcha.replace(/(^\s+)|(\s+$)/g, '');
 
     this.setState({
       ...this.state,
@@ -107,7 +111,7 @@ class BindInfo extends React.Component {
     const { t } = this.props;
     const verifyCode = this.state.verifyCode;
     if (checkEmpty(phone, t('手机号')) || checkEmpty(verifyCode, t('手机验证码'))) {
-      return;
+      return; 
     }
     const data = {};
     data.phone = phone;
@@ -138,6 +142,7 @@ class BindInfo extends React.Component {
     }
   }
   onStartCountDown() {
+    const { t } = this.props;
     let timer = this.state.timer;
     let num = 60;
     const that = this;
@@ -245,7 +250,7 @@ class BindInfo extends React.Component {
   }
 
   shouldCheckboxAgree() {
-    if (window.orgCode === 'mWZdPNwaKg' || window.orgCode === 'kQBeXDWeyK' || window.orgCode === 'joQeZ6JepZ' ||  window.orgCode === 'EKQe1wRbJY') {
+    if (window.orgCode === 'mWZdPNwaKg' || window.orgCode === 'kQBeXDWeyK' || window.orgCode === 'joQeZ6JepZ' || window.orgCode === 'EKQe1wRbJY') {
       return true;
     }
     return false;
@@ -256,31 +261,41 @@ class BindInfo extends React.Component {
       <div className="page-profile-bind-info-container">
         <div className="page-profile-bind-info-container-item">
           <span className="page-profile-bind-info-container-fonts">{t('手机号')}</span>
-          <input className="page-profile-bind-info-container-input" type="tel" ref={(c) => { this.userphone = c; }} onKeyUp={this.onTextChanged} maxLength="11" />
+          <input className="page-profile-bind-info-container-input" placeholder='请输入' name='userphone' type="tel" onChange={(e) => {
+            this.userphone = e.target.value;
+            this.onTextChanged();
+          }} onKeyUp={this.onTextChanged} maxLength="11" />
         </div>
         <div className="line1px" />
         <div className="page-profile-bind-info-container-item">
           <span className="page-profile-bind-info-container-fonts">{t('图片码')}</span>
-          <input className="page-profile-bind-info-container-input" ref={(c) => { this.captcha = c; }} type="text" onKeyUp={this.onTextChanged} />
+          <input className="page-profile-bind-info-container-input" placeholder='请输入' name='captcha' onChange={(e) => {
+            this.captcha = e.target.value;
+            this.onTextChanged();
+          }} type="text" onKeyUp={this.onTextChanged} />
           <img className="page-profile-bind-info-container-code" src={this.state.captchaUrl} onClick={this.refreshCaptcha} alt="" />
         </div>
         <div className="line1px" />
         <div className="page-profile-bind-info-container-item">
           <span className="page-profile-bind-info-container-fonts">{t('验证码')}</span>
-          <input className="page-profile-bind-info-container-input" type="number" ref={(c) => { this.usercode = c; }} onKeyUp={this.onTextChanged} />
+          <input className="page-profile-bind-info-container-input" placeholder='请输入' name='usercode' type="number" onChange={(e) => {
+            this.usercode = e.target.value;
+            this.onTextChanged();
+          }}
+          />
           <div className="page-profile-bind-info-container-code" onClick={this.onSend}>{this.state.buttonString}</div>
         </div>
         <div className="line1px" />
         <div className="page-profile-bind-info-container-submmit" onClick={this.onSubmit}>{t('确认提交')}</div>
         {
-                this.shouldCheckboxAgree() ? this.checkboxAgreeRender() : this.nomarlAgreeRender()
-              }
+          this.shouldCheckboxAgree() ? this.checkboxAgreeRender() : this.nomarlAgreeRender()
+        }
       </div>
 
     );
   }
   onTextMailChanged() {
-    const mail = this.mail.value.replace(/(^\s+)|(\s+$)/g, '');
+    const mail = this.mail.replace(/(^\s+)|(\s+$)/g, '');
 
     this.setState({
       ...this.state,
@@ -306,8 +321,11 @@ class BindInfo extends React.Component {
       <div className="page-profile-bind-info-container">
         <div className="page-profile-bind-info-container-item">
           <span className="page-profile-bind-info-container-fonts">{t('邮箱')}</span>
-          <input className="page-profile-bind-info-container-input" type="email" ref={(c) => { this.mail = c; }} onKeyUp={this.onTextMailChanged} />
-
+          <input className="page-profile-bind-info-container-input" type="email" name='mail' onChange={(e) => {
+            this.mail = e.target.value;
+            this.onTextChanged();
+          }} 
+          />
         </div>
         <div className="line1px" />
         <div className="page-profile-bind-info-container-submmit" onClick={this.onMailSubmit}>{t('确认提交')}</div>
@@ -319,7 +337,9 @@ class BindInfo extends React.Component {
     // const data = this.state.data;
     return (
       <div className="page-profile-bind-container">
-        {this.type === 'phone' ? this.bindPhoneview() : this.bindMailview()}
+        <form action="form">
+          {this.type === 'phone' ? this.bindPhoneview() : this.bindMailview()}
+        </form>
       </div>
     );
   }
